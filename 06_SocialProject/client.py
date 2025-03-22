7d2586e2070d8af3dae15d162bcb0c3b684a2635
Mastah <blackmastah21@gmail.com>
2025-03-22 04:04:09 +0300

    06_SocialProject: finish my netcat client
    Implement say and yield, also add complete for say

--- 06_SocialProject/client.py
+++ 06_SocialProject/client.py
@@ -68,6 +68,33 @@ class CowChatClient(cmd.Cmd):
             return list(self.cow_names)
         return [cow for cow in self.cow_names if cow.startswith(text)]
 
+    def do_say(self, arg):
+        """Send a message to a user: say <user> <message>"""
+        if not self.me:
+            print("Login to send and receive messages.")
+            return
+        args = arg.split()
+        if len(args) < 2:
+            print("Usage: say <user> <message>")
+            return
+        target, message = args[0], ' '.join(args[1:])
+        self.socket.sendall(f'say {target} {message}\n'.encode())
+
+    def complete_say(self, text, line, begidx, endidx):
+        if not text:
+            return list(self.users)
+        return [user for user in self.users if user.startswith(text)]
+
+    def do_yield(self, arg):
+        """Broadcast a message to all users: yield <message>"""
+        if not self.me:
+            print("Login to send and receive messages.")
+            return
+        if not arg:
+            print("Usage: yield <message>")
+            return
+        self.socket.sendall(f'yield {arg}\n'.encode())
+
     def do_quit(self, arg):
         """Quit the chat: quit"""
         self.socket.sendall(b'quit\n')
